7baf452ace4e58077ededc5affee3301cac9f92f
Anthony Catullo <acatullo4@gmail.com>
2021-06-25 14:06:13 -0500

    adding styles

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 
 const cardArray = [1,2,3,4,5,6,7,8,9,10,11]
 const hitButton = document.querySelector('#hit-button')
+const dealButton = document.querySelector('#deal-button')
 const myCards = document.querySelector('#my-cards')
+const dealerCards = document.querySelector('#dealer-cards')
+
+const dealerCardArray = []
 const myCardArray = []
 
 function hitMe() {
@@ -9,5 +13,16 @@ function hitMe() {
     myCards.innerHTML = myCardArray
 }
 
-hitButton.addEventListener('click', hitMe)
+function deal(){
+    myCardArray.push(cardArray[Math.floor(Math.random() * cardArray.length)])
+    myCardArray.push(cardArray[Math.floor(Math.random() * cardArray.length)])
+    myCards.innerHTML = myCardArray
 
+    dealerCardArray.push(cardArray[Math.floor(Math.random() * cardArray.length)])
+    dealerCards.innerHTML = dealerCardArray
+
+    dealButton.removeEventListener('click', deal)
+    hitButton.addEventListener('click', hitMe)
+}
+
+dealButton.addEventListener('click', deal)
