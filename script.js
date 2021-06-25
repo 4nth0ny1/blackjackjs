995e49f8501d8041296cc72021a679c7b01b7e58
Anthony Catullo <acatullo4@gmail.com>
2021-06-25 14:53:40 -0500

    add sum js to player

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 
-const cardArray = [1,2,3,4,5,6,7,8,9,10,11]
 const hitButton = document.querySelector('#hit-button')
 const dealButton = document.querySelector('#deal-button')
 const myCards = document.querySelector('#my-cards')
 const dealerCards = document.querySelector('#dealer-cards')
+const playerSum = document.querySelector('#player-sum')
+const dealerSum = document.querySelector('#dealer-sum')
 
+const cardArray = [1,2,3,4,5,6,7,8,9,10,11]
 const dealerCardArray = []
 const myCardArray = []
+let playerSumTotal = 0
+let dealerSumTotal = 0
 
 function hitMe() {
     myCardArray.push(cardArray[Math.floor(Math.random() * cardArray.length)])
     myCards.innerHTML = myCardArray
+
+    playerSumTotal = 0
+
+    for (let i = 0; i < myCardArray.length; i++) {
+        playerSumTotal += myCardArray[i]
+    }
+
+    playerSum.innerHTML = playerSumTotal
 }
 
 function deal(){
@@ -23,6 +35,23 @@ function deal(){
 
     dealButton.removeEventListener('click', deal)
     hitButton.addEventListener('click', hitMe)
+
+    for (let i = 0; i < myCardArray.length; i++) {
+        playerSumTotal += myCardArray[i]
+    }
+
+    playerSum.append(playerSumTotal)
+   
+    for (let i = 0; i < dealerCardArray.length; i++) {
+        dealerSumTotal += dealerCardArray[i]
+    }
+
+    dealerSum.append(dealerSumTotal)
 }
 
+
+
+
+
 dealButton.addEventListener('click', deal)
+
